9ffc622531ad5693164254b2a1e698fb75a9ff5e
Derek <derek_kwong8@yahoo.com>
2017-01-22 22:34:43 -0800

    updated titles and background

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,3 +1,12 @@
 module ApplicationHelper
 	include SessionsHelper
+
+	def full_title(page_title = '')
+		base_title = "Private Events"
+		if page_title.empty?
+			base_title
+		else
+			page_title + " | " + base_title
+		end
+	end
 end
