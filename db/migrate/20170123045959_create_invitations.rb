ac26ce77705e031d7feb367f6dd559485a100c3b
Derek <derek_kwong8@yahoo.com>
2017-01-22 22:03:46 -0800

    changed invitations migration

--- db/migrate/20170123045959_create_invitations.rb
+++ db/migrate/20170123045959_create_invitations.rb
@@ -1,7 +1,7 @@
 class CreateInvitations < ActiveRecord::Migration[5.0]
   def change
     create_table :invitations do |t|
-      t.references :invitee, foreign_key: true
+      t.references :invitee, foreign_key: true, references: :users
       t.references :event, foreign_key: true
 
       t.timestamps
