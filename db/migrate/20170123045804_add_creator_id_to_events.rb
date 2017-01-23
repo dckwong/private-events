4cec65e2e542998a39fa29ee7f3d2fde51bd7188
Derek <derek_kwong8@yahoo.com>
2017-01-22 21:17:39 -0800

    update to migrations

--- db/migrate/20170123045804_add_creator_id_to_events.rb
+++ db/migrate/20170123045804_add_creator_id_to_events.rb
@@ -1,6 +1,5 @@
 class AddCreatorIdToEvents < ActiveRecord::Migration[5.0]
   def change
-    #add_reference :events, :creator, foreign_key: true, index: true, references: :users
-    add_foreign_key :events, :users, column: :creator_id
-  end
+    add_reference :events, :creator, index: true, foreign_key: true,
+end
 end
