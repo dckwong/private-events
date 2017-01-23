607a58c32b03eeb0c341620991b4ba57580ab839
Derek <derek_kwong8@yahoo.com>
2017-01-22 21:04:51 -0800

    updated creator_index migration

--- db/migrate/20170123045804_add_creator_id_to_events.rb
+++ db/migrate/20170123045804_add_creator_id_to_events.rb
@@ -1,5 +1,5 @@
 class AddCreatorIdToEvents < ActiveRecord::Migration[5.0]
   def change
-    add_reference :events, :creator, foreign_key: true, index: true
+    add_reference :events, :creator, foreign_key: true, index: true, references: :users
   end
 end
